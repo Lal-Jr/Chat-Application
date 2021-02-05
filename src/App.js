207b6f20211b26ba933db851466ab029e7b99687
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2021-02-05 19:36:40 +0530

    Added Messages cComponent and Completed Chat Feed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from "./components/ChatFeed.component.jsx";
+
 import './App.css';
 
 const App = () => {
@@ -9,6 +11,7 @@ const App = () => {
             projectID="574f77cf-2e18-40e6-824c-b2fba73d9a45"
             userName="lal-jr"
             userSecret="123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}></ChatFeed>}
         />
     );
 }
