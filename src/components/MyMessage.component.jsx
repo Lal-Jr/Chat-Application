818ae0d6d0517a649def0d72bc6150b9dcc93607
Harish Lal <69763020+Lal-Jr@users.noreply.github.com>
2021-02-06 16:23:18 +0530

    Completed MyMessage

--- src/components/MyMessage.component.jsx
+++ src/components/MyMessage.component.jsx
@@ -1,7 +1,17 @@
-const MyMessage = () => {
+const MyMessage = ({ message }) => {
+    if( message?.attachements?.length > 0) {
+        return(
+            <img
+                src={message.attachements[0].file}
+                alt="message-attachment"
+                className="message-image"
+                style={{ float: 'right' }}
+            />
+        )
+    }
     return (
-        <div>
-            MyMessage
+        <div classname="message" style={{ float: right, marginRight: '18px', color: 'white', backgroundColor: '#3B3A50'}}>
+            {message.text}
         </div>
     );
 }
